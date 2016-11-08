f9d2193dd98198107c9e7ba64ed953d51019b4cd
Dawid Wyrkowski <dawidwyrkowski@gmail.com>
2016-11-08 17:28:56 +0100

    First initialzation of grunt package

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            files: 'sass/*.sass',
+            tasks: ['sass']
+        },
+        sass: {
+            dev: {
+                files: {
+                    'css/main.css': 'sass/main.sass'
+                }
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        '*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './'
+                }
+            }
+        }
+    });
+
+    // load npm tasks
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    // define default task
+    grunt.registerTask('default', ['browserSync', 'watch']);
+};
